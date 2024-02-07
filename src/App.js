87226e265e10ab468988f281cd757c7d0ce67318
Ivan <aivan.alvarez@gmail.com>
2024-02-06 20:27:45 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepages/homepage';
+import { Route, Routes, useParams, useLocation, Link } from 'react-router-dom';
+
+
+function TopicList() {
+  const { pathname } = useLocation();
+  console.log(pathname);
+  return(
+      <div>
+        <h1>Topic List</h1>
+        <Link to={`${pathname}/233`}>Item 233</Link>
+        <Link to={`${pathname}/213`}>Item 213</Link>
+      </div>
+  )
+}
+
+function TopicDetail() {
+  let { userId } = useParams();
+  return(
+      <div>
+        <h1>Item Detail</h1>
+        <h2>{ userId }</h2>
+      </div>
+  )
+}
+
+const hatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path='/' Component={HomePage} />
+        <Route exact path="/list" Component={TopicList} />
+        <Route path="/list/:userId" Component={TopicDetail} />
+        <Route path="/hats" Component={hatsPage} />
+      </Routes>
     </div>
   );
 }
